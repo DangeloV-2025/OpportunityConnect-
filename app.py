c7b6c38205c34a3cbc300d2ed46e4c79c1ebd163
DangeloV-2025 <Vincent.dangelo@gilmour.org>
2024-10-12 20:16:56 -0400

    Added scholarships page and updated landing page link

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# Define the route for the landing page
 @app.route('/')
-def home():
-    return render_template('home.html')
+def landing():
+    return render_template('landing.html')
+
+# Scholarships page route
+@app.route('/scholarships')
+def scholarships():
+    return render_template('scholarships.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
