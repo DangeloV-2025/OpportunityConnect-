bf192ed576ac8ee62271ab937aee0d7c2e693c85
DangeloV-2025 <Vincent.dangelo@gilmour.org>
2024-11-29 23:17:05 -0500

    added new pages

--- app.py
+++ app.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-# Define the route for the landing page
 @app.route('/')
-def landing():
-    return render_template('landing.html')
+def index():
+    return render_template('index.html')
 
-# Scholarships page route
 @app.route('/scholarships')
 def scholarships():
     return render_template('scholarships.html')
 
+@app.route('/fly-ins')
+def fly_ins():
+    return render_template('fly_ins.html')
+
+@app.route('/precollege')
+def precollege():
+    return render_template('precollege.html')
+
+@app.route('/community')
+def community():
+    return render_template('community.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
